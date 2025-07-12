e74267cb6a5a55631181b611a80e249d7a6e5b01
ay981 <aymenabdulkerim411@gmail.com>
2025-07-12 15:16:14 +0300

    commit

--- task_1/main.ts
+++ task_1/main.ts
@@ -1,8 +1,25 @@
-interface Teacher{
-    readonly first_name:string,
-    readonly last_name: string,
-    fullTimeEmployee : boolean,
-    yearsOfExperience?:number,
-    location : string,
-[key:string]: any
+interface Teacher {
+  readonly first_name: string;
+  readonly last_name: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
 }
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  first_name: 'John',
+  last_name: 'Doe',
+  
+  fullTimeEmployee: true,
+  location: 'London',
+  numberOfReports: 17,
+  contract: false, 
+  yearsOfExperience: 5 
+};
+
+console.log(director1);
